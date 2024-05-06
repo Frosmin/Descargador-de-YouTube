4d4608dde522e2dc17707e68b0c194cc460ddcc6
Frosmin <simonabastomart@gmail.com>
2024-05-05 22:50:14 -0400

    primer comit

---
+++ descargador.py
@@ -0,0 +1,9 @@
+from pytube import YouTube
+
+def descargar_video_youtube(url):
+    youtube = YouTube(url)
+    video = youtube.streams.get_highest_resolution()
+    video.download()
+
+# Usar la función
+descargar_video_youtube('https://www.youtube.com/watch?v=dQw4w9WgXcQ')
